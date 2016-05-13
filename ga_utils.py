57f65956508eeada1477fa8a285f3e003945f3be
Felix Averlant <felix.averlant@gmail.com>
2016-05-13 15:39:00 +0200

    Added docs

--- ga_utils.py
+++ ga_utils.py
@@ -5,18 +5,21 @@ import auth
 service = auth.main()
 
 def list_accounts():
+	"""List all accounts """
 	service = auth.main()
 	accounts = service.management().accounts().list().execute()
 	df = pd.read_json(json.dumps(accounts.get('items')))
 	return df[['id','name']]
 
 def list_properties(account):
+	"""List all properties based on id column """
 	properties = service.management().webproperties().list(
         accountId=account).execute()
 	df = pd.read_json(json.dumps(properties.get('items')))
 	return df[['accountId', 'id' , 'name']]
 
 def list_profiles(account, property):
+	"""List all properties based on id column from list_accounts() & id column form list_properties()"""
 	profiles = service.management().profiles().list(
           accountId=account,
           webPropertyId=property).execute()
@@ -24,6 +27,7 @@ def list_profiles(account, property):
 	return df[['accountId', 'id', 'name']]
 
 def get_example(profile_id):
+	"""Example query based on id column from list_profiles()"""
 	return ga_to_df(service.data().ga().get(
 		ids='ga:' + profile_id,
 		start_date='7daysAgo',
@@ -33,11 +37,13 @@ def get_example(profile_id):
 	).execute())
 
 def ga_to_df(d):
+	"""Take a resp json from GA and transform to pandas DF """
 	columns = [x['name'] for x in d['columnHeaders']]
 	rows = [x for x in d['rows']]
 	return pd.DataFrame(rows, columns=columns)
 
 def raw_query(profile_id, start_date, end_date,metrics,dimensions):
+	""" Utility method to make raw query """
 	return service.data().ga().get(
 		ids='ga:' + profile_id,
 		start_date=start_date,
